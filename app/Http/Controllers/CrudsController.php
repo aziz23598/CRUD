eaf3b5ab8eb4b5355954c4ca6d44f26eaf5a291f
aziz23598 <azizkuncor26@gmail.com>
2022-05-19 13:29:37 +0700

    up

---
+++ app/Http/Controllers/CrudsController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Crud;
+use Illuminate\Http\Request;
+
+class CrudsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Crud::latest()->paginate(5);
+    
+        return view('crud.index',compact('data'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('crud.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required',
+            'detail' => 'required',
+        ]);
+    
+        Crud::create($request->all());
+     
+        return redirect()->route('crud.index')
+                        ->with('success','Data created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Crud $data)
+    {
+        return view('crud.show',compact('data'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $d = Crud::findOrFail($id);
+        return view('crud.edit',compact('d'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'nama' => 'required',
+            'detail' => 'required',
+        ]);
+    
+        Crud::whereId($id)->update($validatedData);
+    
+        return redirect()->route('crud.index')
+                        ->with('success','Data updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $data = Crud::findOrFail($id);
+        $data->delete();
+    
+        return redirect()->route('crud.index')
+                        ->with('success','Data deleted successfully');
+    }
+}
---
+++ resources/views/crud/index.blade.php
@@ -0,0 +1,52 @@
+@extends('crud.layout')
+ 
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Laravel 8 CRUD</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('crud.create') }}"> Create New Data</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+   
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Details</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($data as $d)
+        <tr>
+            <td>{{ ++$i }}</td>
+            <td>{{ $d->nama }}</td>
+            <td>{{ $d->detail }}</td>
+            <td>
+                <form action="{{ route('crud.destroy',$d->id) }}" method="POST">
+   
+                    <a class="btn btn-info" href="{{ route('crud.show',$d->id) }}">Show</a>
+    
+                    <a class="btn btn-primary" href="{{ route('crud.edit',$d->id) }}">Edit</a>
+   
+                    @csrf
+                    @method('DELETE')
+      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+  
+    {!! $data->links() !!}
+      
+@endsection
